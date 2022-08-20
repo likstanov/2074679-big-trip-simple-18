6dd7c333d5e1f5132d32d6aa1d01dfeaec0d4a83
Mikhail Likstanov <likstanov@gmail.com>
2022-08-20 14:53:50 +0300

    add: create view templates

---
+++ src/view/templates/event-view-template.js
@@ -0,0 +1,45 @@
+import { getShortEventDay, getShortEventTime } from '../../utils.js';
+
+const getOffersItemsListTemplate = (offers) => {
+  let listItemsList = '';
+  offers.forEach((offer) => {
+    listItemsList += `
+    <li class="event__offer">
+        <span class="event__offer-title">${offer.title}</span>
+        +€&nbsp;
+        <span class="event__offer-price">${offer.price}</span>
+      </li>
+    `;
+  });
+  return listItemsList ? listItemsList : 'No additional offers';
+};
+
+const createEventTemplate = (event, destination, offers) => (`<li class="trip-events__item">
+  <div class="event">
+    <time class="event__date" datetime="2019-03-18">${getShortEventDay(event.dateFrom)}</time>
+    <div class="event__type">
+      <img class="event__type-icon" width="42" height="42" src="img/icons/${event.type}.png" alt="Event type icon">
+    </div>
+    <h3 class="event__title">${event.type} ${destination.name}</h3>
+    <div class="event__schedule">
+      <p class="event__time">
+        <time class="event__start-time" datetime="2019-03-18T10:30">${getShortEventTime(event.dateFrom)}</time>
+        —
+        <time class="event__end-time" datetime="2019-03-18T11:00">${getShortEventTime(event.dateTo)}</time>
+      </p>
+    </div>
+    <p class="event__price">
+      €&nbsp;<span class="event__price-value">${event.basePrice}</span>
+    </p>
+    <h4 class="visually-hidden">Offers:</h4>
+    <ul class="event__selected-offers">
+      ${getOffersItemsListTemplate(offers)}
+    </ul>
+    <button class="event__rollup-btn" type="button">
+      <span class="visually-hidden">Open event</span>
+    </button>
+  </div>
+</li>
+`);
+
+export { createEventTemplate };
