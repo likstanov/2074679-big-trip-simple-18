ba9d8122e350496b7834b67f5e0de33a1c07639d
Mikhail Likstanov <likstanov@gmail.com>
2022-08-20 14:54:08 +0300

    fix: add model

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { FilterMenuView } from './view/filter-menu-view.js';
 import { EventsPresenter } from './presenter/events-presenter.js';
+import { EventsModel } from './model/events-model.js';
 import { render } from './render.js';
 
 const filtersContainerELement = document.querySelector('.trip-controls__filters');
 const eventsContainerElement = document.querySelector('.trip-events');
 
-const eventsPresenter = new EventsPresenter();
+const eventsModel = new EventsModel();
+const pointsPresenter = new EventsPresenter();
 
 render(new FilterMenuView(), filtersContainerELement);
 
-eventsPresenter.init(eventsContainerElement);
+pointsPresenter.init(eventsContainerElement, eventsModel);
