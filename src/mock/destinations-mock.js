2117402bc9fed7aa12a56108515746e2874e549a
Mikhail Likstanov <likstanov@gmail.com>
2022-08-20 14:51:31 +0300

    add: modules for generating mock data

---
+++ src/mock/destinations-mock.js
@@ -0,0 +1,26 @@
+import { getRandomIntegerFromRange, getRandomElementFromArray } from '../utils.js';
+import { CITIES, DESCRIPTIONS, DESTINATION_PICTURES_AMOUNT } from './const-data.js';
+
+const generateCitiesPictures = () => {
+  const {min, max} = DESTINATION_PICTURES_AMOUNT;
+  const randomPicturesAmount = getRandomIntegerFromRange(min, max);
+  return Array.from({length: randomPicturesAmount}, () => ({
+    src: `http://picsum.photos/300/200?r=${getRandomIntegerFromRange(1,100)}`,
+    description: getRandomElementFromArray(DESCRIPTIONS),
+  }));
+};
+
+const getDestinations = () => {
+  const result = [];
+  CITIES.forEach((city, index) => {
+    result.push({
+      id: index + 1,
+      name: city,
+      description: getRandomElementFromArray(DESCRIPTIONS),
+      pictures: generateCitiesPictures(),
+    });
+  });
+  return result;
+};
+
+export { getDestinations };
---
+++ src/mock/events-mock.js
@@ -0,0 +1,29 @@
+import { getRandomElementFromArray, getRandomIntegerFromRange } from '../utils.js';
+import { EVENT_TYPES, CITIES, POINTS_AMOUNT } from './const-data.js';
+import { getRandomOffersByTypeId } from './offers-mock.js';
+import dayjs from 'dayjs';
+
+const generateDate = (date) => {
+  const gap = getRandomIntegerFromRange(1,4);
+  return dayjs(date).add(gap, 'days').add(gap, 'hours').toISOString();
+};
+
+const getEvents = (amount = POINTS_AMOUNT) => {
+  const result = [];
+  for(let i = 0; i < amount; i++) {
+    const dateFrom = generateDate();
+    const getRandomPointType = getRandomElementFromArray(EVENT_TYPES);
+    result.push({
+      basePrice: getRandomIntegerFromRange(1000, 5000),
+      dateFrom,
+      dateTo: generateDate(dateFrom),
+      destination: getRandomIntegerFromRange(1, CITIES.length),
+      id: i,
+      offers: getRandomOffersByTypeId(getRandomPointType),
+      type: getRandomPointType
+    });
+  }
+  return result;
+};
+
+export { getEvents };
